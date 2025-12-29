b116cbf63f9146d048c773bcd353d324059c44ce
Leo C <70669813+leocebillo@users.noreply.github.com>
2025-12-28 20:41:27 -0600

    Removed

--- contact.php
+++ contact.php
@@ -1,18 +1,2 @@
-<?php
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-    $name = htmlspecialchars($_POST["name"]);
-    $email = htmlspecialchars($_POST["email"]);
-    $message = htmlspecialchars($_POST["message"]);
+voided
 
-    $to = "yourbakery@email.com";
-    $subject = "New Order Inquiry from $name";
-    $body = "Name: $name\nEmail: $email\nMessage:\n$message";
-    $headers = "From: $email";
-
-    if(mail($to, $subject, $body, $headers)){
-        echo "<p>Thank you! Your message has been sent.</p>";
-    } else {
-        echo "<p>Oops! Something went wrong.</p>";
-    }
-}
-?>
